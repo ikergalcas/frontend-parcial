7ade256ce099a97d3eb8022d1882b9ff432236a8
Iker Gálvez Castillo <ikergalcas@gmail.com>
2023-12-15 13:19:50 +0100

    Login

--- src/login/Login.js
+++ src/login/Login.js
@@ -10,17 +10,13 @@ const CompLogin = () => {
     }
 
     useEffect (() => {
-        try{
-            /* global google */
-            google.accounts.id.initialize({
-                client_id: '249081549551-2m7g5cc6fo0vigcp6tk7u76gn43gpqab.apps.googleusercontent.com',
-                callback: handleCallBackResponse
-            });
-        } catch (error) {
-            window.location.href="/login"
-        }
+       
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: '249081549551-2m7g5cc6fo0vigcp6tk7u76gn43gpqab.apps.googleusercontent.com',
+            callback: handleCallBackResponse
+        });
         
-
         /* global google */
         google.accounts.id.renderButton(
             document.getElementById("singInDiv"), {
